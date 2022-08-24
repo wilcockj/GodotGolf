39ef77dc2df6219baa9b8c005643fb155f7f9d00
James Wilcock <jameswilcock8@gmail.com>
2022-08-23 23:23:31 -0700

    added possibly line from golf ball

--- Node2D.gd
+++ Node2D.gd
@@ -16,7 +16,9 @@ func _process(delta):
 func _on_Player_gotloc(startloc, endloc):
 	if startloc:
 		$Line2D.points = []
+		var ballpos = $Player.get_position()
 		$Line2D.add_point(startloc)
+		#$Line2D.add_point(ballpos)
 		$Line2D.add_point(get_viewport().get_mouse_position())
 	if not startloc and not endloc:
 		$Line2D.points = []
